2f3474bed05caeea6d2c85e1758b048928fe9ea6
Aleksi Aalto <aleksi.aalto@student.hamk.fi>
2020-01-13 14:02:18 +0200

    convertteri

--- convertteri.py
+++ convertteri.py
@@ -3,9 +3,17 @@ import sys, os
 from PIL import Image
 
 # valitse ensimmäinen kansio (=Pokedex), jonka jälkeen toinen (=new)
+kuvienKansio = sys.argv[1]
+uudetKuvatKansio = sys.argv[2]
 
 # tarkista onko luotu kansiota nimeltä new ja, jos ei ole, luo se
+if not os.path.exists(uudetKuvatKansio):
+    os.makedirs(uudetKuvatKansio)
 
 # käy pokedex kansio lävitse ja muunna kuvat png muotoon ja tallenna ne new- kansioon
-
+for filename in os.listdir(kuvienKansio):
+    img = Image.open(f'{kuvienKansio}{filename}')
+    siistittyNimi = os.path.splitext(filename)[0]
+    img.save(f'{uudetKuvatKansio}{siistittyNimi}.png', 'png')
+    print("Valmista!")
 
