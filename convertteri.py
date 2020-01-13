197ef7e0663d23f454bf0a2e515e2c06cf695ac9
Aleksi Aalto <aleksi.aalto@student.hamk.fi>
2020-01-13 13:41:37 +0200

    convertterin toiminnalisuuksien listaus

---
+++ convertteri.py
@@ -0,0 +1,11 @@
+# tämä ohjelma muuntaa kuvat jpg- muodosta png-muotoon
+import sys, os
+from PIL import Image
+
+# valitse ensimmäinen kansio (=Pokedex), jonka jälkeen toinen (=new)
+
+# tarkista onko luotu kansiota nimeltä new ja, jos ei ole, luo se
+
+# käy pokedex kansio lävitse ja muunna kuvat png muotoon ja tallenna ne new- kansioon
+
+
