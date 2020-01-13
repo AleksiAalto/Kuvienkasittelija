af4a0539236589eef0cdb26811eb0838065827ff
Aleksi Aalto <aleksi.aalto@student.hamk.fi>
2020-01-13 13:30:09 +0200

    thumbnail ominaisuuden demonstrointi

--- Astronmuokkaus.py
+++ Astronmuokkaus.py
@@ -1,4 +1,7 @@
 from PIL import Image, ImageFilter
 
 img = Image.open('./astro.jpg')
+img.thumbnail((400, 400)) # pitää kuvanlaadun samaisena, kun alkuperäisellä
+#img.save('thumbnail.jpg') 
 
+print(img.size)
